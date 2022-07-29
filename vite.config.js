ba1c8bf0e05eb485d3df10cc38ca4b47d80fec95
Mauro Alfonso <maurogalfonso@gmail.com>
2022-07-29 15:03:05 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://maurogalf.github.io/vite-react-rick-and-morty/",
+  base: "https://maurogalf.github.io/react-vite-rickandmorty/",
   plugins: [react()]
 })
